706a6b57c2bcdd0e23218f6fc483d778085dd136
hamzaouikhalil <hamzaouikhalil.kh@gmail.com>
2024-01-03 13:14:48 +0100

    commit to pull

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -2,7 +2,32 @@ import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 const prisma = new PrismaClient();
 
+interface User {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    image: string;
+    strawberries: number;
+    rate: number;
+    level: string;
+    accountType: boolean;
+    nbrOfDonation: number;
+    nbrOfTakes: number;
+    adress: string;
+    phone: string
+  }  
 
-export const addUser = async()=>{
+export const addUser = async(req: Request, res: Response)=>{
+    try{
+      const {id,firstName,lastName,email, image, adress, phone}: User = req.body
+    //   const userData  = {id, firstName, lastName, email, image,adress , phone} 
+      const query = await prisma.user.create({
+        data: req.body
+      })
+      res.send(query)
+    }catch(error){
+        console.log(error)
+    }
     
 }
