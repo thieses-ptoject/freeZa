edd9b8390548261d0f20bd70faacf8102d14635d
aziza0412 <azizabenhalima@live.fr>
2024-01-03 10:43:46 +0100

    data base

--- server/controllers/adminController.ts
+++ server/controllers/adminController.ts
@@ -3,7 +3,13 @@ import { Request, Response } from "express";
 const prisma = new PrismaClient();
 
 
-export const addAdmin = async(req: Request, res: Response)=>{
-    const{id ,name , image, email}=req.body 
-    
+export const addAdmin = async (req: Request, res: Response) => {
+    const { id, name, image, email } = req.body
+    try { 
+        const admin =
+    }
+    catch (err) {
+
+    }
+
 }
