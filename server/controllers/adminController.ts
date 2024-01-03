b43cb1034b652910ec3480bbd989dd398c63085d
aziza0412 <azizabenhalima@live.fr>
2024-01-03 11:43:04 +0100

    like table

--- server/controllers/adminController.ts
+++ server/controllers/adminController.ts
@@ -5,11 +5,37 @@ const prisma = new PrismaClient();
 
 export const addAdmin = async (req: Request, res: Response) => {
     const { id, name, image, email } = req.body
-    try { 
-        const admin =
+    try {
+        if (id === '') { return res.status(400).send('please give a valide id ') }
+        else if(name===''||image===''||email===''){return  res.status(400).send('please give a valide fields')}
+        else {
+            const admin = await prisma.admin.create({
+                data: {
+                    id, name, image, email
+                },
+            });
+            res.status(200).send(admin)
+        }
     }
     catch (err) {
+        res.status(500).send
 
     }
+
+}
+export const getAdmin = async (req: Request, res: Response) => {
+    const { id } = req.params
+    try { if(id===''|| typeof id==="number"){res.status(500).send("give a valide id")}
+    else {
+     const  admin  =prisma.admin.findUnique({
+     where: {
+      id:id 
+    },
+  })
+  }}
+    catch (err) { 
+        res.status(500).send(err)
+    }
+
 
 }
