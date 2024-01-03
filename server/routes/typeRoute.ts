d7d510d919d36f834c778a1490003dafc21fad5b
nessrineatig <nesrineatig14@gmail.com>
2024-01-03 11:54:29 +0100

    back type

--- server/routes/typeRoute.ts
+++ server/routes/typeRoute.ts
@@ -1,5 +1,7 @@
 import * as typeRouter from "../controllers/typeController"
 
-const route = require("express").Router();
+const route = require("express").Router(); 
+ 
+route.post("/:categoryId",typeRouter.Addtype)
 
 export default route;
