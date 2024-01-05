d441298ebabb13bab07f3d08cd5d428cda3962d5
Wided-Fathallah <widedwed@gmail.com>
2024-01-05 09:26:30 +0100

    befor

--- server/routes/commentRoute.ts
+++ server/routes/commentRoute.ts
@@ -7,4 +7,6 @@ route.get("/getall", commentRouter.getallcommentOfonePost)
 route.put("/update/:userId", commentRouter.updateComment)
 route.delete("/delete/:userId", commentRouter.deleteoneComment)
 route.delete("/deleteall/:userId", commentRouter.deleteAllCommentS)
+
+
 export default route;   
