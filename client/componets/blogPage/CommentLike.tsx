201c602dd4ea158a674c17df6b3faab3c62f8827
aziza0412 <azizabenhalima@live.fr>
2024-01-11 10:22:33 +0100

    blogpage 10%

--- client/componets/blogPage/CommentLike.tsx
+++ client/componets/blogPage/CommentLike.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import { View } from 'react-native'
-
+import React, { useState } from 'react'
+import { View,Text } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const CommentLike = () => {
+  const [color,setColor]=useState(true)
+  const  [commented,setCommented]=useState(true)
   return (
-   <View></View>
+    <View style={{flexDirection:'row'}}>
+    <View style={{flexDirection:'row'}}>
+    {color?<Ionicons name={'heart-outline'} size={20} color={'red'} />:
+    <Ionicons name={'heart'} size={20} color={'red'} />}
+    <Text>1</Text>
+    </View>
+    <View style={{flexDirection:'row'}}>
+    {color?<MaterialCommunityIcons name={'comment-account-outline'} size={20} color={'black'} />:
+    <MaterialCommunityIcons name={'comment-account'} size={20} color={'black'} />}
+    <Text>1</Text>
+    </View>
+   </View>
   )
 }
 
