c1e58849a3e3b3f382853e2a4b0e76c39b290cb1
hamzaouikhalil <hamzaouikhalil.kh@gmail.com>
2024-01-18 09:13:25 +0100

    router

--- ClientWeb/src/App.jsx
+++ ClientWeb/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {
   createBrowserRouter,
@@ -8,12 +6,8 @@ import {
   Route,
   Link,
 } from "react-router-dom";
-import  Admin from "./admin/src/App"
-import ECommerce from './admin/src/./pages/Dashboard/ECommerce';
-import SignIn from './admin/src/./pages/Authentication/SignIn';
-import SignUp from './admin/src/./pages/Authentication/SignUp';
-import Loader from './admin/src/./common/Loader';
-import routes from './admin/src/./routes';
+import Admin from './components/Admin';
+
 function App() {
   
   const router = createBrowserRouter([
