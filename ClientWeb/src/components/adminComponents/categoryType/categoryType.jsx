64ff75bf8daf3d41254b74c15fbb19f489b42a79
hamzaouikhalil <hamzaouikhalil.kh@gmail.com>
2024-01-19 13:15:03 +0100

    to pull

--- ClientWeb/src/components/adminComponents/categoryType/categoryType.jsx
+++ ClientWeb/src/components/adminComponents/categoryType/categoryType.jsx
@@ -1,12 +1,21 @@
 import Header from "./header";
-
+import TypeCategoryList from "./typeCategoryList";
+import { useState } from "react";
+import axios from "axios";
 const CategoryType = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    axios.get();
+  };
   return (
     <div className="h-screen flex">
       <div className="bg-red-100  w-64">sidebar</div>
       <div className="flex flex-col flex-1">
         <Header />
-        <div className="flex-1">Content goes here</div>
+        <div className="flex-1">
+          <TypeCategoryList />
+        </div>
       </div>
     </div>
   );
