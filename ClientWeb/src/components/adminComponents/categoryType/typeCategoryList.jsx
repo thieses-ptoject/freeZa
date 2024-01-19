d70447f088c831fc42d76797d81507036cc6a4cb
hamzaouikhalil <hamzaouikhalil.kh@gmail.com>
2024-01-19 13:04:14 +0100

    to pull

---
+++ ClientWeb/src/components/adminComponents/categoryType/typeCategoryList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TypeCatogoryDetails from "./typeCatogoryDetails";
+
+const TypeCategoryList = () => {
+  return (
+    <div>
+      <TypeCatogoryDetails />
+    </div>
+  );
+};
+
+export default TypeCategoryList;
---
+++ ClientWeb/src/components/adminComponents/categoryType/typeCatogoryDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TypeCatogoryDetails = () => {
+  return <div>TypeCatogoryDetails</div>;
+};
+
+export default TypeCatogoryDetails;
