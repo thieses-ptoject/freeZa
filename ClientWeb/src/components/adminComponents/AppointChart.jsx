cebd9609fc343d864cc5d3b3d1f92b671c5c57b4
nessrineatig <nesrineatig14@gmail.com>
2024-01-22 11:19:59 +0100

    dashboard 90

--- ClientWeb/src/components/adminComponents/AppointChart.jsx
+++ ClientWeb/src/components/adminComponents/AppointChart.jsx
@@ -13,12 +13,32 @@ import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Responsive
         console.error('error fetching data:',error)
         
     }
- }
+ }  
+
+ useEffect(()=>{
+    fetchApp()
+ },[])
 
 
 
   return (
-    <div>AppointChart</div>
+    <div className='h-[25rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1 ml-5'>
+      <strong className='text-gray-700 font-medium'>Types Statistics</strong>
+      <div className='w-full mt-3 flex-1 text-xs'>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart   width={500}
+						height={300}
+                        data={data} margin={{ top: 20, right: 10, left: -10, bottom: 0 }}>
+            <CartesianGrid strokeDasharray="3 3  0 0" vertical={false} />
+            <XAxis dataKey="name"/>
+            <YAxis  />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="value" fill="#ff4040" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
   )
 } 
 
