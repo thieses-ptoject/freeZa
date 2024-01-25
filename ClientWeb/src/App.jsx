f2f4d64840d656b2c4ed146464ab66e0d39937d3
hamzaouikhalil <hamzaouikhalil.kh@gmail.com>
2024-01-25 15:39:32 +0100

    search users admin dashboard improvement input

--- ClientWeb/src/App.jsx
+++ ClientWeb/src/App.jsx
@@ -14,12 +14,8 @@ import "./App.css";
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={
-        <div>
-          <h1 className="text-red-600">Hello World</h1>
-        </div> }
-        />
-        <Route path='/adminLogin'
+
+        <Route path='/'
         element= {
           <SignIn/>
         } 
--- client/secreens/FirstScreens/Password.tsx
+++ client/secreens/FirstScreens/Password.tsx
@@ -35,6 +35,7 @@ const Password = ({ navigation, route }: any) => {
   );
   const [userName, setUserName] = React.useState("");
   const Fauth = getAuth(app);
+  const [view, setView] = React.useState(true);
 
   console.log(banned, "sjqkldjqsdsqdkjqskdljqskdqslkskkkkkkkkkkkkkkkkkkkk");
 
@@ -131,13 +132,22 @@ const Password = ({ navigation, route }: any) => {
             source={require("../../assets/password/bubbles.png")}
           />
           <View style={styles.dots}>
+            <Text style={styles.TextToWritePassword}>Type your password</Text>
             <TextInput
-              secureTextEntry={true}
+              secureTextEntry={view}
               maxLength={32}
               style={styles.ellispse01Icon}
               onChange={(e) => setPassword(e.nativeEvent.text)}
               editable={!banned}
+              placeholder=""
+              placeholderTextColor="#000"
             />
+            <Pressable onPress={() => setView(!view)}>
+              <Image
+                style={{ marginLeft: 190 }}
+                source={require("../../assets/Signup/eyeslash.png")}
+              />
+            </Pressable>
             {errorMessage !== "" && (
               <Text style={styles.errorMessage}>{errorMessage} </Text>
             )}
@@ -175,6 +185,10 @@ const Password = ({ navigation, route }: any) => {
 };
 
 const styles = StyleSheet.create({
+  TextToWritePassword: {
+    position: "absolute",
+    top: -40,
+  },
   errorMessage: {
     width: 500,
     color: "red",
@@ -350,13 +364,18 @@ const styles = StyleSheet.create({
   ellispse01Icon: {
     width: 230,
     left: 0,
-    top: 0,
+    top: -10,
     textAlign: "center",
     height: 30,
     position: "absolute",
-    backgroundColor: "#EC4E4E",
     borderRadius: 100,
-    color: Color.colorWhite,
+    paddingHorizontal: 16,
+    fontSize: 18,
+
+    backgroundColor: "#fff",
+    borderBottomWidth: 2,
+    borderBottomColor: "red",
+    color: "#333",
   },
   ellispse01Icon1: {
     left: 29,
